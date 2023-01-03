3aa00a7d16133e1b149f51491a79e0761f08589c
hojun-kwak <ghwns5618@naver.com>
2023-01-03 11:47:42 +0900

    reset

--- U27_Cpp/Source/U04_Cpp/01_Actor/C02_SpawnActor_Sphere.cpp
+++
@@ -1,9 +0,0 @@
-#include "C02_SpawnActor_Sphere.h"
-#include "Global.h"
-
-AC02_SpawnActor_Sphere::AC02_SpawnActor_Sphere()
-{
-	UStaticMesh* mesh;
-	CHelpers::GetAsset<UStaticMesh>(&mesh, "StaticMesh'/Game/Meshes/M_Sphere.M_Sphere'");
-	Mesh->SetStaticMesh(mesh);
-}
--- U27_Cpp/Source/U04_Cpp/U04_Cpp.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "U04_Cpp.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, U04_Cpp, "U04_Cpp" );
